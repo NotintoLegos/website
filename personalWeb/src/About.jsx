8c708899b18f54c7f22490567fcc85ff956bc448
John Hope <john.hope1997@gmail.com>
2026-07-16 16:43:55 -0500

    added resume and download button to about page

--- personalWeb/src/About.jsx
+++ personalWeb/src/About.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import Header from "./Header";
 //import "about.css";
+import resume from "../public/John-Hope-Resume-PDF.pdf";
 
 export default function About() {
+    function DownloadButton() {
+        return (
+            <a href={resume} download="John-Hope-Resume-PDF.pdf">
+                <button> Download Resume</button>
+            </a>
+        )
+    }
     return (
         <>
         <section id="header">
@@ -14,6 +22,7 @@ export default function About() {
                 <button onClick={() => window.location.href = '/'}>
                     Back to Home
                 </button>
+                <DownloadButton></DownloadButton>
             </div>
         </section>
         </>
